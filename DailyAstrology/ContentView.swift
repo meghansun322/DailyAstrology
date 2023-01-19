8e9b7e65982892ecb07b64b0e4cd13fec59754c6
Meghan Sun <meghansun322@gmail.com>
2023-01-19 15:06:03 -0500

    Initial Commit

---
+++ DailyAstrology/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DailyAstrology
+//
+//  Created by Meghan Sun on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DailyAstrology/DailyAstrologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyAstrologyApp.swift
+//  DailyAstrology
+//
+//  Created by Meghan Sun on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DailyAstrologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
